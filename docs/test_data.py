f4710327055a8f277a67403b99100eadef7be526
Evan Lih <Evan.Lih@Colorado.edu>
2022-01-27 14:27:34 -0800

    Test Treemap

--- docs/test_data.py
+++ docs/test_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sodapy import Socrata
 import plotly.express as px
 import plotly
+import squarify
 client = Socrata("data.seattle.gov", None)
 
 results = client.get("99yi-dthu", limit = 47000)
@@ -26,6 +27,8 @@ results_df['investigation_begin_date'].head()
 results_df= results_df.drop_duplicates(subset = 'file_number', keep = 'first')
 
 results_df = results_df.drop_duplicates(subset = 'file_number', keep = 'first')
+results_df['disposition'].replace({'-':'OPA Active Cases'}, inplace = True)
+
 Dispositions_yearly = pd.DataFrame(results_df.groupby(['disposition', 'year']).size().reset_index(name = "counts")).sort_values('year', inplace = False)
 
 Incidents_yearly = pd.DataFrame(results_df.groupby(['incident_type', 'year']).size().reset_index(name = "counts")).sort_values('year', inplace = False)
@@ -36,20 +39,24 @@ Dispositions_yearly = Dispositions_yearly.loc[Dispositions_yearly['year'] >=2014
 
 Incidents_yearly = Incidents_yearly.loc[Incidents_yearly['year']>=2014]
 
-#Dispositions can only contain No Allegations Sustained, Partially Sustained, All Allegations Sustained, and Rapid Adjudication Closed. ALl the other findings arte sent back to the CoC or 
+#Dispositions can only contain No Allegations Sustained, Partially Sustained, All Allegations Sustained, and Rapid Adjudication Closed. ALl the other findings arte sent back to the CoC or
 
-Dispositions_yearly = Dispositions_yearly[Dispositions_yearly['disposition'].isin(['OPA All Allegations Sustained', 'OPA No Allegations Sustained', 'OPA Partially Sustained', 'OPA Rapid Adjudication Closed', '-'])]
+Dispositions_yearly = Dispositions_yearly[Dispositions_yearly['disposition'].isin(['OPA All Allegations Sustained', 'OPA No Allegations Sustained', 'OPA Partially Sustained', 'OPA Rapid Adjudication Closed', 'OPA Active Cases'])]
 
-Dispositions_yearly = Dispositions_yearly.rename(columns = {"-": "OPA Active Case"})
 
 fig = px.histogram(Dispositions_yearly, x = "disposition", y = "counts", facet_col = "year").update_xaxes(tickangle=30)
 
-fig.show()
+treemap = px.treemap(Dispositions_yearly, path = [ "disposition"], values = 'counts')
+
+
+# treemap.show()
 
 fig1 = px.histogram(Incidents_yearly, x = "incident_type", y = "counts", facet_col = "year").update_xaxes(tickangle=30)
 
 plotly.offline.plot(fig1, filename = 'F:/Github/CPC_Test_Dashboard/docs/histogram.html')
 plotly.offline.plot(fig, filename = 'F:/Github/CPC_Test_Dashboard/docs/histogram2.html')
+plotly.offline.plot(treemap, filename = 'F:/Github/CPC_Test_Dashboard/docs/treemap.html')
+
 
 
 # results_2021.groupby(results_df['received_date'].dt.year)
