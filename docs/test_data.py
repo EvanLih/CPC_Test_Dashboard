1d18148aa26885c0711117f8d782187a7ea32061
Evan Lih <Evan.Lih@Colorado.edu>
2022-02-14 08:33:37 -0800

    Updated treemap to include percentages

--- docs/test_data.py
+++ docs/test_data.py
@@ -26,7 +26,7 @@ results_df['investigation_end_date'].head()
 results_df['investigation_begin_date'].head()
 results_df= results_df.drop_duplicates(subset = 'file_number', keep = 'first')
 
-results_df = results_df.drop_duplicates(subset = 'file_number', keep = 'first')
+# results_df = results_df.drop_duplicates(subset = 'file_number', keep = 'first')
 results_df['disposition'].replace({'-':'OPA Active Cases'}, inplace = True)
 
 Dispositions_yearly = pd.DataFrame(results_df.groupby(['disposition', 'year']).size().reset_index(name = "counts")).sort_values('year', inplace = False)
@@ -34,8 +34,9 @@ Dispositions_yearly = pd.DataFrame(results_df.groupby(['disposition', 'year']).s
 Incidents_yearly = pd.DataFrame(results_df.groupby(['incident_type', 'year']).size().reset_index(name = "counts")).sort_values('year', inplace = False)
 
 
-##creating new subsets from 2019-2021, for flowchart data. 
+##creating new subsets from 2019-2021, for flowchart data. 2019 onward since rappid adjudication wasn't used before then, and from 2017-2018, contact logs were created for anytime someone called OPA. 
 Incident_type_2019_2022 = Incidents_yearly.loc[Incidents_yearly['year'] >=2019]
+Incident_type_2019_2022 = Incident_type_2019_2022.loc[Incident_type_2019_2022['incident_type'] != 'OPA Intake']
 
 Incident_type_2019_2022 
 
@@ -52,19 +53,26 @@ Dispositions_yearly = Dispositions_yearly[Dispositions_yearly['disposition'].isi
 fig = px.histogram(Dispositions_yearly, x = "disposition", y = "counts", facet_col = "year").update_xaxes(tickangle=30)
 
 treemap = px.treemap(Dispositions_yearly, path = ["year","disposition"], values = 'counts')
-totaltreemap = px.treemap(Incident_type_2019_2022 , path = [ "disposition"], values = 'counts')
+totaltreemap = px.treemap(Incident_type_2019_2022 , path = [ "incident_type"], values = 'counts')
+
+totaltreemap.data[0].textinfo = 'label+percent entry'
+totaltreemap.data[0].hovertemplate = '%{label}: %{value} <br> %{counts:.2f}'
 
 totaltreemap.show()
 
 fig1 = px.histogram(Incidents_yearly, x = "incident_type", y = "counts", facet_col = "year").update_xaxes(tickangle=30)
 
-plotly.offline.plot(fig1, filename = 'F:/Github/CPC_Test_Dashboard/docs/histogram.html')
-plotly.offline.plot(fig, filename = 'F:/Github/CPC_Test_Dashboard/docs/histogram2.html')
-plotly.offline.plot(treemap, filename = 'F:/Github/CPC_Test_Dashboard/docs/treemap.html')
-plotly.offline.plot(totaltreemap, filename = 'F:/Github/CPC_Test_Dashboard/docs/totaltreemap.html')
+plotly.offline.plot(fig1, filename = 'F:/Github/CPC_Test_Dashboard/docs/images/histogram.html')
+plotly.offline.plot(fig, filename = 'F:/Github/CPC_Test_Dashboard/docs/images/histogram2.html')
+plotly.offline.plot(treemap, filename = 'F:/Github/CPC_Test_Dashboard/docs/images/treemap.html')
+plotly.offline.plot(totaltreemap, filename = 'F:/Github/CPC_Test_Dashboard/docs/images/totaltreemap.html')
 
 
 
 
-# results_2021.groupby(results_df['received_date'].dt.year)
+# Begin Use of Force Dataset pull 
+client = Socrata("data.seattle.gov", None)
+useOfForce = client.get('ppi5-g2bj', limit = 47000)
+useOfForce = pd.DataFrame.from_records(useOfForce)
 
+useOfForce.head
